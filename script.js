b11861273d804b29435f215d2eeacdd62f1e1384
Fengxing WEI <86094248+fxwei@users.noreply.github.com>
2022-11-17 16:49:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function formatted(feed,item, s){
     let T = new Date(item.pubDate);
     let t = [`0${(T.getHours()+8)%24}`.slice(-2),`0${T.getMinutes()}`.slice(-2)];
     block += `<div class="item ${s}"><button type="button" class="collapsible ">
-    <a href="${item.link}" target="_blank"><div class="summary">[${t[0]}:${t[1]}]
-    </a>${item.title}</div><div class="category">${feed.title.replace('rthk.hk - 即時新聞: ','')}</div></button><div class="content"><p>${item.description}</p></div></div>`;
+    <a href="${item.link}" target="_blank"><div class="summary">${t[0]}:${t[1]}
+    </a> ${item.title}</div><div class="category">${feed.title.replace('rthk.hk - 即時新聞: ','')}</div></button><div class="content"><p>${item.description}</p></div></div>`;
     return {time:T, news:block};
 }
 
@@ -40,7 +40,7 @@ $( document ).ajaxStop(function() {
   for(let n of newsSeq){
     document.getElementById('show').insertAdjacentHTML('beforeend', n.news);
   }
-  
+  document.getElementById('show').style.display="block";
   var coll = document.getElementsByClassName("collapsible");
   var i;
 
@@ -48,11 +48,26 @@ $( document ).ajaxStop(function() {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      if (content.style.maxHeight){
-        content.style.maxHeight = null;
+      if (content.style.height){
+        content.style.height = null;
       } else {
-        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.height = content.scrollHeight + "px";
       }
     });
   }
+
+    var filt = document.getElementsByClassName("filter");
+    var j;
+
+      for (j = 0; j < filt.length; j++) {
+        filt[j].addEventListener("click", function() {
+          let selectedNews = document.getElementsByClassName(`item ${this.classList[1]}`);
+          if (this.classList.toggle("selected")){
+            for(s of selectedNews) s.style.display="block";
+          } else {
+            for(s of selectedNews) s.style.display="none";
+          }
+        });
+      }
 })
+
